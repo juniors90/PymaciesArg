5bdda8fb3f3160023c1acd06742c768eaa667e1c
juniors90 <juandavid9a0@gmail.com>
2022-10-14 20:54:51 -0300

    FIX setup,py

--- setup.py
+++ setup.py
@@ -42,8 +42,7 @@ with open("README.md") as fp:
     LONG_DESCRIPTION = fp.read()
 
 with open(PATH / "requirements" / "pypharmsarg.txt") as fp:
-    for line in fp.readlines():
-        REQUIREMENTS = fp.split("\n")
+    REQUIREMENTS = fp.read().strip().split("\n")
 
 with open(PATH / "requirements" / "dev.txt") as fp:
     dev_requires = fp.read().strip().split("\n")
